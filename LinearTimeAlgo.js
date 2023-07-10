78a873aa90aba9333fd602ddc1a19dc2c3037377
Ronsou <93101540+SouCode@users.noreply.github.com>
2023-07-10 19:39:05 -0400

    Create LinearTimeAlgo.js

---
+++ LinearTimeAlgo.js
@@ -0,0 +1,11 @@
+function sumArrayElements(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+    console.log(sum);
+  }
+  return sum;
+}
+const numbers = [1, 2, 3, 4, 5]
+const result = sumArrayElements(numbers);
+console.log(result);
