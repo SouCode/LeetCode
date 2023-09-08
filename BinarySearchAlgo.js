27f3bb9cf664009e8430b0f76a436fd577327354
Sou <samelsonjohn@protonmail.com>
2023-09-07 23:39:41 -0400

    Binary Search training

--- BinarySearchAlgo.js
+++ BinarySearchAlgo.js
@@ -1,4 +1,5 @@
-// Binary search example 
+//Binary search example 1
+/* 
 function binarySearch(arr, target) {
     let left = 0;
     let right = arr.length - 1;
@@ -43,3 +44,49 @@ const bookTitles = ['The Jungle Book', 'Naruto', '1984', 'Nobunaga'];
 const targetBook = 'Nobunaga' 
 const bookResult = binarySearch2(bookTitles, targetBook);
 console.log(bookResult);
+*/
+
+// Example #2
+
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] === target) {
+            return mid;  // Found the target, return its index
+        } else if (arr[mid] < target) {
+            left = mid + 1;  // Target is in the right half
+        } else {
+            right = mid - 1;  // Target is in the left half
+        }
+    }
+
+    return -1;  // Target not found in the array
+}
+
+const bookTitles = [
+    "A Brief History of Time",
+    "Clean Code",
+    "Design Patterns",
+    "Eloquent JavaScript",
+    "How to Win Friends and Influence People",
+    "Moby Dick",
+    "The Art of Computer Programming",
+    "The Great Gatsby",
+    "The Pragmatic Programmer",
+    "To Kill a Mockingbird"
+];
+
+const titleToFind = "The Pragmatic Programmer";
+
+const index = binarySearch(bookTitles, titleToFind);
+
+if (index !== -1) {
+    console.log(`Found "${titleToFind}" at index ${index}.`);
+} else {
+    console.log(`"${titleToFind}" not found in the list.`);
+}
+
