5535a8bc1b1b5cee760499d3693f7ce1fae883b4
Sou <samelsonjohn@protonmail.com>
2023-10-02 23:25:39 -0400

    Hash table scenerio

--- HashTableAlgo.js
+++ HashTableAlgo.js
@@ -19,3 +19,41 @@ console.log(hashTable['orange']);
 delete hashTable['banana'];
 console.log(hashTable['banana']);
 
+/* Question
+Given an array of integers, return indices of the 
+two numbers such that they add up to a specific target. 
+You may assume that each input would have exactly one solution, 
+and you may not use the same element twice.
+
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+*/
+
+function twoSum(nums, target) {
+    // Create a hashtable (in JavaScript, it's an object or a Map)
+    const hashtable = new Map();
+    
+    // Iterate through the array with index `i` and value `num`
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        // Calculate the complement of `num` with respect to `target`
+        const complement = target - num;
+        
+        // Check if the complement is in the hashtable
+        if (hashtable.has(complement)) {
+            // If found, return the indices of the two numbers
+            return [hashtable.get(complement), i];
+        }
+        
+        // Otherwise, add `num` and its index to the hashtable
+        hashtable.set(num, i);
+    }
+    
+    // Return an empty array if no solution is found (though it's guaranteed to have a solution per the problem statement)
+    return [];
+}
+
+// Test the function with the example
+console.log(twoSum([2, 7, 11, 15], 9));  // Output: [0, 1]
