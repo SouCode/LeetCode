02b426afa223b3113a19f12c701a25849609e1a0
Sou <samelsonjohn@protonmail.com>
2023-10-05 00:09:57 -0400

    simpler way to exctract jeez

--- CodeWars/DomainExtract.js
+++ CodeWars/DomainExtract.js
@@ -6,16 +6,35 @@
 */
 
 function domainName(url){
+    //if the url does not start with 'http://' or 'https://'.
     // Prepend 'http://' to the URL if it doesn't have a protocol
     if (!url.startsWith('http://') && !url.startsWith('https://')) {
       url = 'http://' + url;
     }
-  
-    const hostname = new URL(url).hostname; // Get the hostname from the URL
-    const domain = hostname.startsWith('www.') ? hostname.slice(4) : hostname; // Remove www if it exists
-    return domain.split('.')[0]; // Take the first part of the domain
+
+    const hostname = new URL(url).hostname; //Creates a URL object, Get the hostname from the URL
+    const domain = hostname.startsWith('www.') ? hostname.slice(4) : hostname; // Remove www. if it exists
+
+
+    // Split the domain string into an array of substrings using '.' as the separator
+    // This results in the array ["example", "com"]
+    // Then, access the first element of the array, which is "example"
+    return domain.split('.')[0]; // This will return "example"
+
   }
   
 console.log(domainName("http://github.com/carbonfive/raygun"));
 console.log(domainName("http://www.zombie-bites.com"));         
 console.log(domainName("https://www.cnet.com"));  
+
+//simpler way lol
+function simplerDomainName(url){
+    return url.replace('http://', '').replace('https://', '').replace('www.', '').split('.')[0];
+  }
+
+console.log(simplerDomainName("http://github.com/carbonfive/raygun"));
+console.log(simplerDomainName("http://www.zombie-bites.com"));         
+console.log(simplerDomainName("https://www.cnet.com"));  
+  
+
+  
